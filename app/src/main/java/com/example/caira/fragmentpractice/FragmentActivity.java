b0a0115c88e9b8f6249d4c6811e81953b071edba
Caira <cairacshields@gmail.com>
2016-07-11 22:59:51 -0500

    New File

--- app/src/main/java/com/example/caira/fragmentpractice/FragmentActivity.java
+++ app/src/main/java/com/example/caira/fragmentpractice/FragmentActivity.java
@@ -7,6 +7,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FragmentActivity extends AppCompatActivity {
 
@@ -35,6 +41,21 @@ public class FragmentActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
             View rootView = inflater.inflate(R.layout.my_fragment, container, false);
+            ListView LV = (ListView)rootView.findViewById(R.id.list_view);
+
+            String[] names ={
+                    "Hailey",
+                    "Blake",
+                    "Valery",
+                    "Winter",
+                    "Mel",
+                    "James"
+            };
+            List<String> list = new ArrayList<>(
+                    Arrays.asList(names)
+            );
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), R.layout.my_fragment, R.id.text_view, list);
+            LV.setAdapter(arrayAdapter);
             return rootView;
         }
     }
