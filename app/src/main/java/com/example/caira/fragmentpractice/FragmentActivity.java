cc379838d9ff1ccf5c5755a5e0b49f85957e6ee3
Caira <cairacshields@gmail.com>
2016-07-11 22:31:13 -0500

    New File

---
+++ app/src/main/java/com/example/caira/fragmentpractice/FragmentActivity.java
@@ -0,0 +1,41 @@
+package com.example.caira.fragmentpractice;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        PlaceholderFragment newFragment = new PlaceholderFragment();
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, newFragment)
+                    .commit();
+        }
+    }
+
+    public static class PlaceholderFragment extends Fragment{
+
+        public PlaceholderFragment(){
+
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.my_fragment, container, false);
+            return rootView;
+        }
+    }
+}
