f245aefa2bfc6d1577d0c1426de22a22de94655f
C'Aira Shields <cairacshields@gmail.com>
2016-07-11 21:39:40 -0500

    Update FragmentActivity.java

--- app/src/main/java/com/example/caira/fragmentpractice/FragmentActivity.java
+++ app/src/main/java/com/example/caira/fragmentpractice/FragmentActivity.java
@@ -18,23 +18,31 @@ public class FragmentActivity extends AppCompatActivity {
         PlaceholderFragment newFragment = new PlaceholderFragment();
 
         if(savedInstanceState == null) {
+            //Begin Fragment Manager Transaction 
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, newFragment)
+                    //Remember to commit the changes to the fragment 
                     .commit();
         }
     }
-
+    
+    
+    //Inner Fragment class extends Fragment 
     public static class PlaceholderFragment extends Fragment{
 
+        //Empty constructor for initilization 
         public PlaceholderFragment(){
 
         }
 
+        //Remember onCreateView is called whenever the Fragment Transaction begins 
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+            //Inflate our rootView Layout 
             View rootView = inflater.inflate(R.layout.my_fragment, container, false);
+            //Once inflated, we return it to our transaction Manager 
             return rootView;
         }
     }
